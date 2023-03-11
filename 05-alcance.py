9a100b9cc122175a560cbbb669fefe4d496af475
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-11 19:28:30 -0300

    added and scope exercise

---
+++ 05-alcance.py
@@ -0,0 +1,12 @@
+#saludo ="Hola global"#variables globales son una malapractica 
+
+def saludar():#siempre definir la variable antes y despues llamarla.
+    saludo = "Hola mundo"
+    print(saludo)
+    
+
+def saludaChanchito():
+    saludo = "Hola Chanchito"
+    print(saludo)
+
+saludar()
